d4407e6fdb6ea14d92476579824b6f073236213e
cmstexas <crystal.m.soria@gmail.com>
2019-07-10 17:10:52 -0500

    refactoring old project

--- Article/Article.js
+++ Article/Article.js
@@ -1,39 +1,102 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement = domElement;
     
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelectorAll('.expandButton');
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton = this.domElement.querySelectorAll('.expandButton');
 
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
 
-    this.expandButton.forEach((button) => {
-      button.textContent = 'expand';
-    })
+//     this.expandButton.forEach((button) => {
+//       button.textContent = 'expand';
+//     })
     
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    //expanding the articles upon clicking "expand"//
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     //expanding the articles upon clicking "expand"//
     
-    this.expandButton.forEach((button) => {
-      button.addEventListener('click', (event) => {
-        this.expandArticle(event);
-      })
-    })
-  }
+//     this.expandButton.forEach((button) => {
+//       button.addEventListener('click', (event) => {
+//         this.expandArticle(event);
+//       })
+//     })
+//   }
+
+
+
+// const article = document.querySelectorAll('.article');
+
+
+// panelData.forEach(data => {
+//   console.log('creating panel:', data.title)
+//   accordion.appendChild(createPanel(data.title, data.content))
+// })
+
+//define new elements
+
+function createArticleComponent() {
+  const article = document.createElement('div')
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const paragraphOne = document.createElement('p');
+  const paragraphTwo = document.createElement('p');
+  const paragraphThree = document.createElement('p');
+  const buttonPanel = document.createElement('div');
+  const expandButton = document.createElement('span');
+  // const buttonOpen = document.createElement('button');
+  // const buttonClose = document.createElement('button');
+
   
+//setup structure of elements
 
-  expandArticle(){
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-        //TOGGLE the articles//
+articles.appendChild(article)
+article.appendChild(articleTitle)
+article.appendChild(articleDate)
+article.appendChild(paragraphOne)
+article.appendChild(paragraphTwo)
+article.appendChild(paragraphThree)
+article.appendChild(expandButton)
+
+//set class names
+
+article.classList.add('article')
+articleDate.classList.add('date')
+expandButton.classList.add('expandButton')
+
+// set text content
+buttonOpen.textContent = 'Open'
+buttonClose.textContent = 'Close'
+articleContent.textContent = content
+articleTitle.textContent = title
 
 
-    console.log('toggle success');
-    this.domElement.classList.toggle('article-open');
-  }
+// button events
+
+expandButton.addEventListener('click', event => {
+  console.log('button clicked', event.target)
+  // 1. toggle hide-btn on BOTH buttons
+  buttonOpen.classList.toggle('hide-btn')
+  buttonClose.classList.toggle('hide-btn')
+  // 2. Change visibility of the content w/ 'toggle-on'
+  article.classList.toggle('toggle-on')
+})
+
+return article
 }
+
+
+
+//   expandArticle(){
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//         //TOGGLE the articles//
+
+
+//     console.log('toggle success');
+//     this.domElement.classList.toggle('article-open');
+//   }
+// }
 
 /* START HERE: 
 
@@ -43,21 +106,25 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article');
+// let articles = document.querySelectorAll('.article');
+
 // console.log(articles);
 
- articles.forEach((article) => {
-  return new Article(article);
-})
+// OLD
+//  articles.forEach((article) => {
+//   return new Article(article);
+// })
+
+
 
 
 
 //STRETCH -- Add read button and make them all disapear on click - changed to selectAll//
 
-    const buttonDisapear = document.querySelectorAll('.readbutton');
-    buttonDisapear.forEach((readbutton) => {
-        readbutton.addEventListener('click', (event) => {
-        event.target.style.visibility = 'hidden';
-        event.stopPropagation;
-    });
-})
+//     const buttonDisapear = document.querySelectorAll('.readbutton');
+//     buttonDisapear.forEach((readbutton) => {
+//         readbutton.addEventListener('click', (event) => {
+//         event.target.style.visibility = 'hidden';
+//         event.stopPropagation;
+//     });
+// })
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -14,3 +14,4 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', toggleMenu);
+console.log('click', menuButton)
